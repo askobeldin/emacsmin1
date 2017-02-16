f09548aefdead96aa942a536ac1c50fe2001ea1f
askobeldin <askobeldin@gmail.com>
2017-02-16 21:42:52 +0300

    customisation some faces

--- custom.el
+++ custom.el
@@ -11,4 +11,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(Info-quoted ((t (:background "ivory" :height 100 :family "DejaVu Sans Mono")))))
+ '(Info-quoted ((t (:background "ivory" :height 100 :family "DejaVu Sans Mono"))))
+ '(font-lock-comment-face ((t (:foreground "gray40"))))
+ '(font-lock-string-face ((t (:foreground "dark red"))))
+ '(font-lock-type-face ((t (:foreground "medium blue"))))
+ '(font-lock-variable-name-face ((t (:foreground "magenta4")))))
