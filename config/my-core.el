dcf967316547074e5e2f842b19a073a917d56cca
askobeldin <askobeldin@gmail.com>
2017-02-21 17:17:02 +0300

    clipboard in win

---
+++ config/my-core.el
@@ -0,0 +1,71 @@
+;; my-core.el
+;;
+;; core config
+;;
+
+;; Если не отменить make-backup-files, Emacs будет засорять
+;; файловую систему бэкапами.
+(setq-default make-backup-files nil)
+
+;; Это включает автосохранение - пока вы работаете с файлом,
+;; Emacs время от времени автоматически создает копию, и удаляет её
+;; как только вы сохрание файл с которым работаете.
+(setq-default auto-save-defaults t)
+(setq auto-save-interval 500
+      auto-save-timeout 0)
+
+;; Директория для автосохранения файлов
+(setq backup-directory-alist
+          `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+          `((".*" ,temporary-file-directory t)))
+
+;; Это отключает табы
+(setq-default indent-tabs-mode nil)
+
+;; lockfiles are evil.
+(setq create-lockfiles nil)
+
+;; require a trailing newline
+(setq require-final-newline t)
+
+;'Woman' > 'man'.
+(defalias 'man 'woman)
+
+(eval-when-compile (require 'cl))
+(require 'cl-lib)
+
+;; from <https://github.com/bling/dotemacs/>
+(defmacro after (feature &rest body)
+  "After FEATURE is loaded, evaluate BODY."
+  (declare (indent defun))
+  `(eval-after-load ,feature
+     '(progn ,@body)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; CEDET
+; (require 'cedet) ;; использую "вшитую" версию CEDET. Мне хватает...
+; (add-to-list 'semantic-default-submodes 'global-semanticdb-minor-mode)
+; (add-to-list 'semantic-default-submodes 'global-semantic-mru-bookmark-mode)
+; (add-to-list 'semantic-default-submodes 'global-semantic-idle-scheduler-mode)
+; (add-to-list 'semantic-default-submodes 'global-semantic-highlight-func-mode)
+; (add-to-list 'semantic-default-submodes 'global-semantic-idle-completions-mode)
+; (add-to-list 'semantic-default-submodes 'global-semantic-show-parser-state-mode)
+; (semantic-mode   t)
+; (global-ede-mode t)
+; (require 'ede/generic)
+; (require 'semantic/ia)
+; (ede-enable-generic-projects)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(provide 'my-core)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; https://www.emacswiki.org/emacs/AutoInsertMode
+;
+; (auto-insert-mode)  ;;; Adds hook to find-files-hook
+; (setq auto-insert-directory "~/.mytemplates/") ;; Or use custom, *NOTE* Trailing slash important
+; (setq auto-insert-query nil) ;; If you don't want to be prompted before insertion
+; (define-auto-insert "\.py" "my-python-template.py")
+; (define-auto-insert "\.php" "my-php-template.php")
---
+++ config/my-sly.el
@@ -0,0 +1,47 @@
+;; my-sly.el
+;;
+
+(require 'sly)
+(require 'sly-autoloads)
+(require 'sly-company)
+
+(require 'evil-lispy)
+
+
+;; Это включает алгоритмы выравнивания лиспового кода из SLIME,
+;; в противоположность стандартным из Emacs
+; (setq lisp-indent-function 'common-lisp-indent-function)
+
+(setq inferior-lisp-program (executable-find "sbcl"))
+(if (my-system-is-windows)
+    (progn
+      (message "%s" "my-sly: system is windows.")
+      (setq common-lisp-hyperspec-root "file:///c:/users/a.skobeldin/HyperSpec/"))
+    (progn
+      (message "%s" "my-sly: system is linux.")
+      (setq common-lisp-hyperspec-root "file:///usr/share/doc/hyperspec/")))
+
+; electric mode
+; (add-hook 'sly-mrepl-mode-hook 'electric-pair-mode)
+(add-hook 'sly-mrepl-mode-hook #'evil-lispy-mode)
+
+(setq sly-net-coding-system 'utf-8-unix)
+
+(setq sly-contribs '(sly-autodoc sly-fancy))
+
+(add-hook 'sly-mode-hook 'sly-company-mode)
+(add-to-list 'company-backends 'sly-company)
+
+(define-key company-active-map (kbd "\C-n") 'company-select-next)
+(define-key company-active-map (kbd "\C-p") 'company-select-previous)
+(define-key company-active-map (kbd "\C-d") 'company-show-doc-buffer)
+(define-key company-active-map (kbd "M-.") 'company-show-location)  
+
+
+;; Sly modes
+(after 'evil
+(evil-set-initial-state 'sly-db-mode 'insert)
+(evil-set-initial-state 'sly-mrepl-mode 'insert))
+
+
+(provide 'my-sly)
