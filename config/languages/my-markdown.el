f03fcdf63303ecabc86c853d813d3e9762061823
askobeldin <askobeldin@gmail.com>
2017-02-24 23:22:12 +0300

    mymarkdown changed

--- config/languages/my-markdown.el
+++ config/languages/my-markdown.el
@@ -1,17 +1,6 @@
 ;; my-markdown.el
 ;; Settings for editing Markdown files.
 
-; (use-package markdown-mode
-  ; :commands markdown-mode
-  ; :ensure markdown-mode
-  ; :init
-  ; (progn
-    ; (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-    ; (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-    ; (add-to-list 'auto-mode-alist '("\\.page\\'" . markdown-mode))
-    ; (add-hook 'markdown-mode-hook 'visual-line-mode)
-    ; )
-  ; )
 
 (use-package markdown-mode
   :ensure t
@@ -19,7 +8,10 @@
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "multimarkdown"))
-
+  :init
+  (progn
+    (setq markdown-command "multimarkdown")
+    (add-hook 'markdown-mode-hook (lambda ()
+                                     (set-fill-column 80)))))
 
 (provide 'my-markdown)
