8464dc0902b1eac02e0139a037b48e51becf456b
askobeldin <askobeldin@gmail.com>
2017-02-27 21:45:31 +0300

    trying to config my-snippets

--- config/languages/my-snippets.el
+++ config/languages/my-snippets.el
@@ -2,7 +2,29 @@
 ;;
 ;; Code snippets for speedy snipping.
 
+;; try to use popup
+;; (require 'popup)
+
 (use-package yasnippet
-  :ensure yasnippet)
+  :ensure yasnippet
+  ;; :init
+  ;; (progn
+  ;;   (defun yas-popup-isearch-prompt (prompt choices &optional display-fn)
+  ;;       (when (featurep 'popup)
+  ;;           (popup-menu*
+  ;;           (mapcar
+  ;;           (lambda (choice)
+  ;;               (popup-make-item
+  ;;               (or (and display-fn (funcall display-fn choice))
+  ;;                   choice)
+  ;;               :value choice))
+  ;;           choices)
+  ;;           :prompt prompt
+  ;;           ;; start isearch mode immediately
+  ;;           :isearch t))))
+  ;;   :config
+  ;;   (progn
+  ;;     (setq yas-prompt-functions '(yas-popup-isearch-prompt yas-ido-prompt yas-no-prompt)))
+    )
 
 (provide 'my-snippets)
